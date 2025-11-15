2e81aff56007a5ae3fb151c6433887c47f9f3f11
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-11-15 16:23:20 -0300

    criando rota e controller de usuários

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 
+import { routes } from "./routes";
+
 import "express-async-errors";
 import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 app.use(express.json()); // permite receber JSON no corpo da requisição.
 
+app.use(routes);
 app.use(errorHandling);
 
 export { app };
