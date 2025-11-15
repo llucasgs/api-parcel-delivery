21f3c85b1e50a2315a578fed3a8e9b484d17af33
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-11-15 15:40:30 -0300

    configurando error-handling e AppError

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+
+export function errorHandling(
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+}
