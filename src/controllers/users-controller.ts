80dbc9920c5686479145733598d248859664d5b6
Lucas Garcia e Silva <llucasgarciaesilva@hotmail.com>
2025-11-20 14:54:06 -0300

    validando dados do usuário com bodyschema no método get

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   create = (request: Request, response: Response) => {
+    const bodySchema = z.object({
+      name: z.string({ required_error: "name is required!" }).trim().min(2),
+      email: z.string({ required_error: "e-mail is required!" }).email(),
+      password: z.string({ required_error: "password is required!" }).min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
     return response.json({ message: "ok" });
   };
 }
